ba615fc2bf179782ac568b0915e569366d173ce2
Lenka Yaswith Krishna Vamsi <107264238+yaswith10@users.noreply.github.com>
2023-07-13 12:40:08 +0530

    Add files via upload
    adding table components

---
+++ tables2/src/App.js
@@ -0,0 +1,11 @@
+import {LiveScoresComponent} from "./LiveScoresComponent"
+
+function App() {
+  return (
+    <div className="App">
+      <LiveScoresComponent/>
+    </div>
+  );
+}
+
+export default App;
